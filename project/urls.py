55f1c0f0fb3daa07f5277b404e97d4222c6ea0cb
khayratdinov <khayratdinov.np@gmail.com>
2023-09-25 20:14:09 +0500

    feat: 👥 add custom user model, setup redoc api docs

--- project/urls.py
+++ project/urls.py
@@ -1,21 +1,27 @@
-from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Authors API",
+        default_version="v1",
+        description="API endpoints for the Authors API course",
+        contact=openapi.Contact(email="api.imperfect@gmail.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
     path(settings.ADMIN_URL, admin.site.urls),
 ]
 
-
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-    
-if settings.DEBUG_TOOLBAR:
-    import debug_toolbar
-    urlpatterns = [
-        path("__debug__/", include(debug_toolbar.urls)),
-    ] + urlpatterns
+admin.site.site_header = "Authors Haven API Admin"
+admin.site.site_title = "Authors Haven API Admin Portal"
+admin.site.index_title = "Welcome to the Authors Haven API Portal"
