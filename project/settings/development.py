291003c9867e13d538230eaddcc659c74fbfddd6
khayratdinov <khayratdinov.np@gmail.com>
2023-11-13 22:21:56 +0500

    🛠 updated: celery

--- project/settings/development.py
+++ project/settings/development.py
@@ -48,6 +48,6 @@ if importlib.util.find_spec("debug_toolbar"):
 EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
 EMAIL_HOST = env("EMAIL_HOST", default="mailhog")
 EMAIL_PORT = env("EMAIL_PORT")
-DEFAULT_FROM_EMAIL = "info@authors-haven.com"
+DEFAULT_FROM_EMAIL = "support@apiimperfect.site"
 DOMAIN = env("DOMAIN")
 SITE_NAME = "Authors Haven"
